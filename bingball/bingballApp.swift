b71120d67763748f6c305dbc8858777f8aa78dbc
wglee0511 <ranazang@naver.com>
2023-09-05 08:22:10 +0900

    Initial Commit

---
+++ bingball/bingballApp.swift
@@ -0,0 +1,17 @@
+//
+//  bingballApp.swift
+//  bingball
+//
+//  Created by racoon on 2023/09/05.
+//
+
+import SwiftUI
+
+@main
+struct bingballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
